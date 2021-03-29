e77e8f384835bafba9ff3b0190ce7fb60ec412cb
Daniel Souza <daniel.souza@diletec.com.br>
2021-03-29 16:06:06 -0300

    Arquivos originais

---
+++ vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-develop',
+    'version' => 'dev-develop',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3db0d9ce0b65bc132d812de8a1863b6c3bccb9bf',
+    'name' => 'rankmath/seo-by-rank-math',
+  ),
+  'versions' => 
+  array (
+    'a5hleyrich/wp-background-processing' => 
+    array (
+      'pretty_version' => '1.0.2',
+      'version' => '1.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2cbee1abd1b49e1133cd8f611df4d4fc5a8b9800',
+    ),
+    'cmb2/cmb2' => 
+    array (
+      'pretty_version' => 'v2.9.0',
+      'version' => '2.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cacbc8cedbfdf8ffe0e840858e6860f9333c33f2',
+    ),
+    'donatj/phpuseragentparser' => 
+    array (
+      'pretty_version' => 'v1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '246c1cf0a44f07168c702203bf30d5f48f17bab0',
+    ),
+    'mythemeshop/wordpress-helpers' => 
+    array (
+      'pretty_version' => 'v1.1.10',
+      'version' => '1.1.10.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '67121692c9fb45f57d05f46dab551e99d3b3b31a',
+    ),
+    'rankmath/seo-by-rank-math' => 
+    array (
+      'pretty_version' => 'dev-develop',
+      'version' => 'dev-develop',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3db0d9ce0b65bc132d812de8a1863b6c3bccb9bf',
+    ),
+    'woocommerce/action-scheduler' => 
+    array (
+      'pretty_version' => '3.1.6',
+      'version' => '3.1.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '275d0ba54b1c263dfc62688de2fa9a25a373edf8',
+    ),
+  ),
+);
